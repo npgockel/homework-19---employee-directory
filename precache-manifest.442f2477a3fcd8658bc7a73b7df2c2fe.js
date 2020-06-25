50fc8fd63d426577d763db35c947a6c04a6a1247
Nectarios UofA-TUC-FSF-PT-02-2020-U-C <npgockel@gmail.com>
2020-06-24 22:10:32 -0700

    Se baila si

---
+++ precache-manifest.442f2477a3fcd8658bc7a73b7df2c2fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b55647647aa38c6244e0b37445f5a26f",
+    "url": "/homework-19---employee-directory/index.html"
+  },
+  {
+    "revision": "ffc933b9fb9103b9b427",
+    "url": "/homework-19---employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d3b92328170b0286cdca",
+    "url": "/homework-19---employee-directory/static/js/2.41aedd85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homework-19---employee-directory/static/js/2.41aedd85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffc933b9fb9103b9b427",
+    "url": "/homework-19---employee-directory/static/js/main.5f809465.chunk.js"
+  },
+  {
+    "revision": "1052ddb380dd7b0882c9",
+    "url": "/homework-19---employee-directory/static/js/runtime-main.6fbf600f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homework-19---employee-directory/static/media/logo.5d5d9eef.svg"
+  }
+]);
